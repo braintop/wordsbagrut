582dc5eae426c66f2998bbdf6b41b0d5def89444
braintop <braintop7@gmail.com>
2025-05-16 13:01:39 +0300

    add 1

--- src/Pages/Favorites/FavoritesPage.tsx
+++ src/Pages/Favorites/FavoritesPage.tsx
@@ -11,7 +11,7 @@ export default function FavoritesPage() {
     const { startQuiz } = useQuiz();
     const { currentUser } = useAuth();
     const navigate = useNavigate();
-    const x = 1; 
+
     const [lists, setLists] = useState<FavoriteList[]>([]);
     const [loading, setLoading] = useState(true);
     const [error, setError] = useState<string | null>(null);
